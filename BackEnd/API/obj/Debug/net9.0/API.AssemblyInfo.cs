855bc5fea7f7ce9df42ca4ab294e64ffb119d204
Shajjad002 <shajjd002@gmail.com>
2026-04-16 15:05:03 +0600

    connit

--- BackEnd/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ BackEnd/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d408263f94dadc725af6cdbc8ecfc92de23fd9c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5023168f489ee7ee3dff2cdd8fc6133dc2082cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
